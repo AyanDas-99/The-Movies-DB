f699a7030dcd84b95c4dce21eb043c176480764f
Ayan Das <ayandas9531@gmail.com>
2022-11-06 22:34:24 +0530

    error msg added

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
@@ -31,59 +31,103 @@ for (i = 0; i < coll.length; i++) {
 }
 
 
-form.addEventListener('submit', async(event)=>{
-    event.preventDefault();
-    displayPreSetup();
-    const movie = await getData(title.value, parseInt(year.value));
-    detailsDiv.style.display = 'block';
+// Event listener
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  displayPreSetup();
+  const movie = await getData(title.value, parseInt(year.value));
+  if (notFound(movie)) {
+    displayError();
+  }
+  else {
     displaySetup(movie);
+  }
 });
 
-const getData = async(title, year)=>{
-    console.log(title, year);
-    let url;
-    if(isNaN(year)){
-        url = `http://www.omdbapi.com/?t=${title}&apikey=97fabfbf`
-    }
-    else{
-        url = `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=97fabfbf`
-    }
-    let f = await fetch(url)
-    return await f.json();
+
+// Fetch movie data
+const getData = async (title, year) => {
+  console.log(title, year);
+  let url;
+  if (isNaN(year)) {
+    url = `http://www.omdbapi.com/?t=${title}&apikey=97fabfbf`
+  }
+  else {
+    url = `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=97fabfbf`
+  }
+  let f = await fetch(url)
+  return await f.json();
 }
 
-const displayPreSetup = ()=>{
-    mainDiv.classList.add('postSearch')
-    const gif = document.createElement('img');
-    gif.id = 'loaderGif';
-    gif.src = 'media/Spinner-1s-200px.gif'
-    gif.style.display = 'block'
-    gif.style.margin = '2em auto'
-    document.body.appendChild(gif);
+
+//Display Setup for search
+const displayPreSetup = () => {
+  detailsDiv.style.display = 'none'
+  mainDiv.classList.add('postSearch')
+  const gif = document.createElement('img');
+  gif.id = 'loaderGif';
+  gif.src = 'media/Spinner-1s-200px.gif'
+  gif.style.display = 'block'
+  gif.style.margin = '2em auto'
+  document.body.appendChild(gif);
 }
 
+
+// Display movie data
 function displaySetup(movie) {
-    let gif = document.getElementById('loaderGif');
-    document.body.removeChild(gif);
-    
-    console.log(movie);
-    poster.src = movie.Poster;
-    titleDisplay.firstElementChild.textContent = movie.Title;
-    mainRating.textContent = movie.imdbRating;
-    displayGenre(movie.Genre);
-    plot.textContent = movie.Plot;
-    director.textContent = movie.Director;
-    writer.textContent = movie.Writer;
-    cast.textContent = movie.Actors;
+  document.body.removeChild(document.getElementById('loaderGif'));
+  try{
+    document.body.removeChild(document.getElementById('error-msg'));
+  }
+  catch(e){
+     
+  }
+  detailsDiv.style.display = 'block';
+  console.log(movie);
+  poster.src = movie.Poster;
+  titleDisplay.firstElementChild.textContent = movie.Title;
+  mainRating.textContent = movie.imdbRating;
+  displayGenre(movie.Genre);
+  plot.textContent = movie.Plot;
+  director.textContent = movie.Director;
+  writer.textContent = movie.Writer;
+  cast.textContent = movie.Actors;
 }
 
+
+// display movie genre
 function displayGenre(genre) {
-    genreDiv.innerHTML = ''
-    const genres = genre.split(',');
-    genres.forEach((element)=>{
-        const span = document.createElement('span')
-        span.className = 'genreItem';
-        span.textContent = element.trim();
-        genreDiv.appendChild(span)
-    })
+  genreDiv.innerHTML = ''
+  const genres = genre.split(',');
+  genres.forEach((element) => {
+    const span = document.createElement('span')
+    span.className = 'genreItem';
+    span.textContent = element.trim();
+    genreDiv.appendChild(span)
+  })
+}
+
+
+// check if movie not found
+function notFound(movie) {
+  return movie.Response === 'False';
+}
+
+// Display not found error
+function displayError() {
+  const errorDiv = document.createElement('div');
+  errorDiv.id = 'error-msg';
+  errorDiv.style.width = '100%'
+  errorDiv.style.textAlign = 'center'
+  errorDiv.style.color = 'grey'
+  errorDiv.style.fontSize = 'large';
+  errorDiv.style.marginTop = '3em'
+  const errorIcon = document.createElement('img');
+  errorIcon.src = 'media/computer.png';
+  errorDiv.appendChild(errorIcon);
+  const errMsg =document.createElement('h4');
+  errMsg.textContent = 'Requested movie not found';
+  errorDiv.appendChild(errMsg);
+  document.body.appendChild(errorDiv);
+  document.body.removeChild(document.getElementById('loaderGif'));
 }
